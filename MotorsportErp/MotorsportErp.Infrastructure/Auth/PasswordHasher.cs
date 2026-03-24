e4eacf07958486e0efeebf18c0ccd71ca2b84147
Daniil Belyaev <daniil.belyaev@reksoft.com>
2026-03-24 20:05:49 +0300

    Change hasher

--- MotorsportErp/MotorsportErp.Infrastructure/Auth/PasswordHasher.cs
+++ MotorsportErp/MotorsportErp.Infrastructure/Auth/PasswordHasher.cs
@@ -1,22 +1,51 @@
 ﻿using MotorsportErp.Application.Interfaces.Security;
 using System.Security.Cryptography;
-using System.Text;
 
 namespace MotorsportErp.Infrastructure.Auth;
 
 public class PasswordHasher : IPasswordHasher
 {
+    private const int SaltSize = 16; // 128 bit
+    private const int KeySize = 32;  // 256 bit
+    private const int Iterations = 10000;
+
     public string Hash(string password)
     {
-        var bytes = Encoding.UTF8.GetBytes(password);
-        var hash = SHA256.HashData(bytes);
+        using var algorithm = new Rfc2898DeriveBytes(
+            password,
+            SaltSize,
+            Iterations,
+            HashAlgorithmName.SHA256);
 
-        return Convert.ToBase64String(hash);
+        var salt = algorithm.Salt;
+        var key = algorithm.GetBytes(KeySize);
+
+        var result = new byte[SaltSize + KeySize];
+
+        Buffer.BlockCopy(salt, 0, result, 0, SaltSize);
+        Buffer.BlockCopy(key, 0, result, SaltSize, KeySize);
+
+        return Convert.ToBase64String(result);
     }
 
     public bool Verify(string password, string hash)
     {
-        var hashedInput = Hash(password);
-        return hashedInput == hash;
+        var bytes = Convert.FromBase64String(hash);
+
+        var salt = new byte[SaltSize];
+        var key = new byte[KeySize];
+
+        Buffer.BlockCopy(bytes, 0, salt, 0, SaltSize);
+        Buffer.BlockCopy(bytes, SaltSize, key, 0, KeySize);
+
+        using var algorithm = new Rfc2898DeriveBytes(
+            password,
+            salt,
+            Iterations,
+            HashAlgorithmName.SHA256);
+
+        var keyToCheck = algorithm.GetBytes(KeySize);
+
+        return keyToCheck.SequenceEqual(key);
     }
 }
